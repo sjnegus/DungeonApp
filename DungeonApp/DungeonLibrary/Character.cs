3605979d88768453aaef52f1c2893b64c0f13fcc
Sam Negus <sam.negus@outlook.com>
2023-11-29 10:20:40 -0600

    weapon and character

---
+++ DungeonApp/DungeonLibrary/Character.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonLibrary
+{
+    internal class Character
+    {
+        private int _life;
+        private string _name;
+        private int _hitChance;
+        private int _block;
+        private int _maxLife;
+
+        public string Name
+        { 
+            get { return _name; } 
+            set { _name = value; }
+        }
+
+        public int HitChance
+        { 
+            get { return _hitChance; }
+            set { _hitChance = value; } 
+        }
+        
+        public int Block
+        { 
+            get { return _block; } 
+            set {  _block = value; } 
+        }
+
+        public int MaxLife
+        { 
+            get { return _maxLife; } 
+            set { _maxLife = value; } 
+        }
+
+        public int Life
+        { 
+            get { return _life; }
+            set
+            {
+                if (value <= MaxLife)
+
+                    _life = value;
+                else
+                {
+                    _life = MaxLife;
+                }
+            }
+        }
+
+        public CHaracter(string name, int maxlife, int hitChance, int block, int life)
+        {
+            Name = name;
+            MaxLife = maxlife;
+            HitChance = hitChance;  
+            Block = block;  
+            Life = life;
+        }
+
+        public override string ToString()
+        {
+            //return base.ToString();
+            return string.Format($"{Name}\nHP: {Life}/{MaxLife}\nHit Chance: {HitChance}\nBlock: {Block}");
+        }
+
+    }
+}
---
+++ DungeonApp/DungeonLibrary/weapon.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonLibrary
+{
+    public class weapon
+    {
+        private int _minDamage;
+        private int _maxDamage;
+        private string _name;
+        private int _bonusHitChance;
+        private bool _isTwoHanded;
+
+        public int MaxDamage
+        { 
+            get { return _maxDamage; } 
+            set { _maxDamage = value; } 
+        }
+
+        public string Name
+        { 
+            get { return _name; }
+            set {  _name = value; } 
+        }
+
+        public int BonusHitChance
+        { 
+            get { return _bonusHitChance; } 
+            set { _bonusHitChance = value; } 
+        }
+
+        public bool IsTwoHanded 
+        { 
+            get {  return _isTwoHanded; } 
+            set { _isTwoHanded = value; } 
+        }
+
+        public int MinDamage
+        {  
+            get { return _minDamage; }
+            set 
+            {
+                if (value > 0 && value <= MaxDamage)
+                {
+                    _minDamage = value;
+                } 
+                else
+                {
+                    _minDamage = 1;
+                }
+            } 
+        }
+
+        public Weapon(string name, int maxDamage, int minDamage, int bonusHitChance, bool isTwoHanded)
+        {
+            Name = name;
+            MaxDamage = maxDamage;
+            MinDamage = minDamage;
+            BonusHitChance = bonusHitChance;
+            IsTwoHanded = isTwoHanded;
+        }
+
+        public override string ToString() 
+        {
+            return string.Format($"{Name}\nDamage: {MinDamage} - {MaxDamage}\nBonus Hit: {BonusHitChance}\n" +
+                $"Type: {(IsTwoHanded ? "Two Handed" : "One Handed")}")
+        }
+
+
+    }
+
+    
+}
